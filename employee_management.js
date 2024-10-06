12d02d871e76979c833f909c5df8f048d772b3a9
Nguyễn Phương Linh <ngplinh2005@gmail.com>
2024-10-06 17:58:57 -0400

    Create Department class to manage employees and calculate salary

--- employee_management.js
+++ employee_management.js
@@ -6,8 +6,21 @@ class Employee {
       this.position = position
       this.department = department
     }
-  
     getDetails() {
       return `Employee Name: ${this.name}, Position: ${this.position}, Salary: $${this.salary}`
     }
   }
+
+// Task 2: Create a Department Class
+class Department {
+    constructor(name) {
+      this.name = name
+      this.employees = []
+    }
+    addEmployee(employee) {
+      this.employees.push(employee)
+    }
+    getDepartmentSalary() {
+      return this.employees.reduce((total, employee) => total + employee.salary, 0)
+    }
+}
